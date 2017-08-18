18520d627e76b5f0facc7324525274c01f71e932
matan <matan.markind@gmail.com>
2017-08-18 16:36:43 +0300

    meh

--- junk.cc
+++ junk.cc
@@ -1,11 +1,23 @@
+// Example program
 #include <iostream>
-#include <string>
 #include <type_traits>
+#include <string>
+#include <vector>
+#include "BunchQueue.hh"
 
-int main()
-{
-  std::cout << std::is_trivially_move_constructible<int>::value << " "
-            << std::is_trivially_move_constructible<std::string>::value << " "
-            << std::endl;
+struct S {
+    int a;
+    bool b;
+    char c[9];
+};
+
+
+int main() {
+  std::string s = "hello, there";
+  matan::VecQueue<std::string> v;
+  v.push_back(s);
+  v.emplace_back("hello, world");
+  v.push_back(std::move(s));
+  std::cout << *v.begin() << "______" << s << std::endl;
+  std::cout << std::is_trivially_copyable<S>::value << "    " << std::is_trivially_move_constructible<S>::value << std::endl;
 }
-
